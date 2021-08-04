541df89dade70881eda53352dfc5d661f822cac7
ribauandre <ribauandre@yahoo.com>
2021-08-04 01:09:06 -0400

    setting grid spacing and putting in background snowfall

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Intro from './components/Intro.js';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
+import Snowfall from 'react-snowfall'
 
 function App() {
   return (
     <Container component="main" maxWidth="sm">
+     <Snowfall snowflakeCount={7}/>
         <CssBaseline />
         <Box my={1}>
             <Intro/>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import Typography from '@material-ui/core/Typography';
 import Avatar from '@material-ui/core/Avatar';
-import avatar from '../imgs/avatar.jpg';
-import Box from '@material-ui/core/Box';
+import avatar from '../imgs/avatar.jfif';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,27 +11,30 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    color: "#fff"
+    color: "#fff",
+    flexGrow: 1,
   },
   large: {
       width: theme.spacing(30),
       height: theme.spacing(30),
+
   },
 }));
 
 export default function Intro() {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
-        <Box my={2}>
-
+  <div className={classes.root}>
+    <Grid container direction="column" justifyContent="space-between" alignItems="center" spacing={7}>
+        <Grid item xs>
             <Avatar alt="Andre Ribau" src={avatar} className={classes.large} />
-
+        </Grid>
+        <Grid item xs>
             <Typography component="h1" variant="h5">
-                Site Reliability Engineer / Software Engineer
+                SRE / SWE
             </Typography>
-
-        </Box>
-    </div>
+        </Grid>
+    </Grid>
+  </div>
   );
 }

ad8f2230e392bc72ffec47babd1937f721e2f912
ribauandre <ribauandre@yahoo.com>
2021-08-04 17:30:11 -0400

    added website content and styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import './App.css';
 import Intro from './components/Intro.js';
+import StickyFooter from './components/StickyFooter.js';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
-import Snowfall from 'react-snowfall'
+import Snowfall from 'react-snowfall';
+import '@fontsource/roboto';
 
 function App() {
   return (
-    <Container component="main" maxWidth="sm">
-     <Snowfall snowflakeCount={7}/>
+    <Container component="main">
+        <Snowfall snowflakeCount={7} color="#7a8b96" />
         <CssBaseline />
-        <Box my={1}>
-            <Intro/>
-        </Box>
+        <Intro/>
+        <StickyFooter />
     </Container>
   );
 }
---
+++ src/components/StickyFooter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import { FaGithubSquare } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaExternalLinkSquareAlt } from "react-icons/fa";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: "center",
+  },
+  footer: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(8),
+    alignItems: "center",
+    color: "#fff",
+    flexGrow: 1,
+
+  },
+}));
+
+export default function StickyFooter() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="h2" component="h5">
+            <a href="https://github.com/Ribauandre"><FaGithubSquare /></a>
+            <a href="https://www.linkedin.com/in/ribauandre"><FaLinkedin /></a>
+            <a href="https://triplebyte.com/tb/andre-ribau-uk5qixs"><FaExternalLinkSquareAlt /></a>
+          </Typography>
+        </Container>
+      </footer>
+    </div>
+  );
+}

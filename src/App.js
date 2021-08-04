d57af9ddfe0a035ae02efbd5886213b44ad23438
ribauandre <ribauandre@yahoo.com>
2021-08-03 22:38:26 -0400

    inti

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Intro from './components/Intro.js';
+import Box from '@material-ui/core/Box';
+
+function App() {
+  return (
+    <div className="App">
+    <Box>
+        <Intro/>
+    </Box>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import avatar from '../imgs/avatar.jpg';
+
+class Intro extends Component {
+
+    render() {
+
+        return (
+            <div>
+            <h1>
+                testiing
+                </h1>
+            </div>
+        )
+    }
+}
+export default Intro
